aa97477503760ac08dec50a4fa182982545e9467
roccopaccione <leonardo.rocco.paccione@gmail.com>
2021-02-26 01:19:57 -0600

    feat: added variables

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -6,7 +6,25 @@ namespace MyConsoleApp
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            OperatorExamples();
+        }
+
+        private static void OperatorExamples()
+        {
+            int width = 3;
+
+            width++;
+
+            int height = 2 + 4;
+            int area = width * height;
+            Console.WriteLine(area);
+
+            string result = "The area";
+            result = result + " is " + area;
+            Console.WriteLine(result);
+
+            bool truthValue = true;
+            Console.WriteLine(truthValue);
         }
     }
 }
